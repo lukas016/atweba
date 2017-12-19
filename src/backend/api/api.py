f084b5231ee648f2055776a65330c219eb461dc6
Lukas Koszegy <xkosze00@stud.fit.vutbr.cz>
2017-12-19 23:11:00 +0100

    testing methods

--- src/backend/api/api.py
+++ src/backend/api/api.py
@@ -1,7 +1,16 @@
 from server import app
 import json
-from flask import jsonify
+from flask import jsonify, request
 
 @app.route('/api/version')
+@app.route('/api/')
 def apiVersion():
     return jsonify(version="0.1")
+
+@app.route('/api/jobs', methods=['GET', 'POST'])
+def routeJobs():
+    if request.method == 'POST':
+        app.logger.debug(request.values)
+        return jsonify(request.files)
+    if request.method == 'GET':
+        return jsonify()
