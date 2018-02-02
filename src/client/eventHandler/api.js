dc9e0904c4383040eb3abe13a0dc384cab6be7a4
Lukas Koszegy <xkosze00@stud.fit.vutbr.cz>
2018-02-02 23:40:26 +0100

    client: add createEvent mutation

--- src/client/eventHandler/api.js
+++ src/client/eventHandler/api.js
@@ -10,12 +10,16 @@ class Api {
     send(msg) {
         this.socket.open("POST", this.serverAddr + this.url);
         this.socket.setRequestHeader("Content-Type", "application/json");
-        msg.id = this.id;
-        let msgForServer = { query: msg,
+        msg.scenarioId = this.id;
+        let msgString = JSON.stringify(msg)
+                .replace(/\"([^(\")"]+)\":/g,"$1:")
+                .substr(1)
+                .slice(0, -1);
+        let msgForCreateEvent = { query: "mutation {createEvent(" + msgString + ") {ok}}",
                          variables: null,
                          operationName: null
                        }
-        this.socket.send(JSON.stringify(msgForServer));
+        this.socket.send(JSON.stringify(msgForCreateEvent));
     }
 }
 
