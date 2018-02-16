058874bfad5d0a8845235c8af0334803243660e4
Lukas Koszegy <xkosze00@stud.fit.vutbr.cz>
2018-02-16 16:37:55 +0100

    backend: centralize start of threads

--- src/backend/main.py
+++ src/backend/main.py
@@ -4,22 +4,32 @@ from aggregator import Aggregator
 import time
 from sys import exit
 from manager.manager import TestManager
+from threading import Thread
 import logging
+
 
 def initLogger():
     FORMAT = '%(asctime)-15s %(name)s %(levelname)s: %(message)s'
     logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 
+def initApiServer():
+    from api.server import apiServer
+    apiServer.debug = True
+    apiServer.use_reloader = True
+    apiServer.run(port=5900, threaded=True)
+
+def startThreads(threads):
+    for thread in threads:
+        thread.start()
+
+def joinThreads(threads):
+    for thread in threads:
+        thread.join()
+        threads.remove(thread)
+
 if __name__ == '__main__':
     initLogger()
-
-    aggregator = Aggregator()
-    aggregator.start()
-
-    manager = TestManager()
-    manager.start()
-
-    from api.server import apiServer
-
-    apiServer.run(port=5900, threaded=True)
+    threads = [Aggregator(), TestManager(), Thread(target=initApiServer)]
+    startThreads(threads)
+    joinThreads(threads)
     exit(0)
