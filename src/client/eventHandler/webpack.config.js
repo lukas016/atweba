3b545b2a619d342561a17adb478f7065ca5417ee
Lukas Koszegy <xkosze00@stud.fit.vutbr.cz>
2018-02-21 00:05:10 +0100

    client: init yarn
    * install webpack

---
+++ src/client/eventHandler/webpack.config.js
@@ -0,0 +1,26 @@
+let path = require('path')
+let webpack = require('webpack')
+
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+
+module.exports = {
+    entry: "./registrator.js",
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: "client.js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+    }]},
+    plugins: [
+        new MinifyPlugin()
+    ]
+}
