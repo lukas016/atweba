d724e2a953c0f262d1757140b0ec9fa5007492f8
Lukas Koszegy <xkosze00@stud.fit.vutbr.cz>
2018-02-23 19:02:58 +0100

    backend: check scenarioId before creating event

--- src/backend/database/elasticsearch.py
+++ src/backend/database/elasticsearch.py
@@ -5,23 +5,29 @@ class ElasticsearchClient():
     def __init__(self, host="127.0.0.1", port=9200, ssl=False):
         self.db = self.connect(host, port, ssl)
         self.manageIndex = 'manage'
+        self.manageDocType = '_doc'
 
     def connect(self, host, port, ssl):
         return Elasticsearch(['{}:{}'.format(host, port)],
                 use_ssl=ssl)
 
     def createEvent(self, msg):
+        if not self.existScenario(msg['scenarioId']):
+            raise Exception('Invalid scenario name '+ msg['scenarioId'])
+
         result = self.db.index(index=msg['scenarioId'], doc_type='tweet', body=msg)
         return result['_shards']['failed'] != 0
 
+    def existScenario(self, scenario):
+        return self.db.exists(index=self.manageIndex, id=scenario, doc_type=self.manageDocType)
+
     def createScenario(self, msg):
-        docType = '_doc'
         id = msg['id']
-        if self.db.exists(index=self.manageIndex, doc_type=docType, id=id):
+        if self.existScenario(id):
             raise Exception('Invalid name ' + id + ': name exist')
 
         del msg['id']
-        result = self.db.index(index=self.manageIndex, doc_type=docType, id=id, body=msg);
+        result = self.db.index(index=self.manageIndex, doc_type=self.manageDocType, id=id, body=msg);
         return result['_shards']['failed'] == 0
 
     def delete(self, type, msg): pass
