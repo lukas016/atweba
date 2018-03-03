01867079b63dab8911029590f9ee7c8de7f94d58
Lukas Koszegy <xkosze00@stud.fit.vutbr.cz>
2018-03-03 22:00:39 +0100

    backend: install chrome driver for selenium

--- src/backend/init.sh
+++ src/backend/init.sh
@@ -3,3 +3,7 @@
 virtualenv -p python3.6 .
 source ./bin/activate
 pip3.6 install -r stable-req.txt
+wget 'https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip'
+unzip chromedriver_linux64.zip
+mv chromedriver bin/
+rm -f chromedriver_linux64.zip
