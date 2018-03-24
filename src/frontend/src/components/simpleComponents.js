50b9377e39b29e0baecd83508c3ab56430e9dd4e
Lukas Koszegy <xkosze00@stud.fit.vutbr.cz>
2018-03-24 23:11:25 +0100

    frontend: create filter for range of date

--- src/frontend/src/components/simpleComponents.js
+++ src/frontend/src/components/simpleComponents.js
@@ -6,6 +6,18 @@ const semanticFilter = ({filter, onChange}, type='text') => (
             onChange={event => onChange(event.target.value)} />
 )
 
+const semanticDateRangeFilter = ({filter, onChange}, obj) => (
+    <div>
+        <Input name='start' className='dateRange' type='date'
+                value={filter && filter.value[1] === 'start' ? filter.value[0] : obj.state.startDate}
+                onChange={event => onChange([event.target.value, 'start'])} />
+        <Input className='dateRange' type='date'
+                value={filter && filter.value[1] === 'end' ? filter.value[0] : obj.state.endDate}
+                onChange={event => onChange([event.target.value, 'end'])} />
+    </div>
+)
+
 export {
-    semanticFilter
+    semanticFilter,
+    semanticDateRangeFilter
 }
