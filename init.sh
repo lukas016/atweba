83dafedac2754aad88635cf66aa28d7405f0006d
Lukas Koszegy <xkosze00@stud.fit.vutbr.cz>
2018-03-31 15:46:13 +0200

    frontend: fix migration PropTypes to separate library in react-image-diff
    * Use own repaired library

--- init.sh
+++ init.sh
@@ -7,6 +7,7 @@ cd ${ACTUAL_DIR}/src/backend
 
 cd ${ACTUAL_DIR}/src/frontend
 yarn
+cp ${ACTUAL_DIR}/src/external/react-image-diff/react-image-diff.js node_modules/react-image-diff/dist/
 
 cd ${ACTUAL_DIR}/src/client/eventHandler/
 yarn
