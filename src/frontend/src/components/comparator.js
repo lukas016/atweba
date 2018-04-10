1cd1190a27e7ce97a89d53256205a6f2bec08433
Lukas Koszegy <xkosze00@stud.fit.vutbr.cz>
2018-04-10 17:04:21 +0200

    frontend: scale ImageDiff in range 0.25 - 1.5

--- src/frontend/src/components/comparator.js
+++ src/frontend/src/components/comparator.js
@@ -6,16 +6,23 @@ import '../css/comparator.css'
 class Comparator extends Component {
     constructor(props) {
         super(props)
-        this.state = {range: 0.5, type: 'difference'}
+        this.state = {range: 0.5, zoom: 1.0, type: 'difference'}
         this.type = ['fade', 'difference', 'swipe'].map(x => ({key: x, text: x, value: x}))
     }
 
     changeRange = (e, {value}) => this.setState({range: parseFloat(value)})
 
+    changeZoom = (e, {value}) => this.setState({zoom: parseFloat(value)})
+
     changeType = (e, {value}) => this.setState({type: value})
 
     render() {
-        const {range, type} = this.state
+        const {range, type, zoom} = this.state
+
+        const styleZoom = {
+            transform: `scale(${zoom})`
+        }
+
         return (
             <div>
                 <div className='controlPanel'>
@@ -23,8 +30,12 @@ class Comparator extends Component {
                         onChange={this.changeType}/>
                     <Input disabled={type === 'difference'} type='range' min={0} max={1} value={range} step={0.01}
                         onChange={this.changeRange}/>{range}
+                    <Input type='range' min={0.25} max={1.5} value={zoom} step={0.01}
+                        onChange={this.changeZoom}/>{zoom}
                 </div>
+            <div style={styleZoom}>
             <ImageDiff before={this.props.before} after={this.props.after} type={type} value={range} />
+            </div>
             </div>
         )
     }
